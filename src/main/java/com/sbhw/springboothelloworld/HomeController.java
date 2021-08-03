3500ecf9634b525c153ed92f30f452287674a8eb
Wonki Lee <chuirang@naver.com>
2021-08-03 09:51:22 +0900

    SpringBoot Hello World Project Created.

---
+++ src/main/java/com/sbhw/springboothelloworld/HomeController.java
@@ -0,0 +1,13 @@
+package com.sbhw.springboothelloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping(value="/home", method=RequestMethod.GET)
+	public String index() {
+		return "Hello World with Spring Boot";
+	}
+}
